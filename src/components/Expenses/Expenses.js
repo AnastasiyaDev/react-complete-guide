966b5eeaa9e14486703c2c04493abf542f05ef1b
Asya Bakhnar <abakhnar@oggettoweb.com>
2021-09-27 01:26:13 +0300

    Restrucure and refactore Expenses components and Card

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,14 +1,17 @@
+import React from 'react';
+
 import ExpenseItem from './ExpenseItem';
+import Card from '../UI/Card';
 
 import './Expenses.css';
 
-function Expenses(props) {
+const Expenses = (props) => {
     return (
-        <div>
+        <Card className="expenses">
             <ExpenseItem title={props.items[0].title} date={props.items[0].date} amount={props.items[0].amount} />
             <ExpenseItem title={props.items[1].title} date={props.items[1].date} amount={props.items[1].amount}/>
             <ExpenseItem title={props.items[2].title} date={props.items[2].date} amount={props.items[2].amount}/>
-        </div>
+        </Card>
     );
 }
 export default Expenses;
