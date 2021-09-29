ad14e417f4e9321f3f3ea0176786640f41d71437
Asya Bakhnar <abakhnar@oggettoweb.com>
2021-09-30 00:24:15 +0300

    Add my version ExpensesFilter

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,13 +1,19 @@
 import React from 'react';
 
+import ExpensesFilter from './ExpensesFilter';
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 
 import './Expenses.css';
 
 const Expenses = (props) => {
+    const filterExpensesHandler = (option) => {
+        console.log(option)
+    };
+
     return (
         <Card className="expenses">
+            <ExpensesFilter defaultYear="2020" onFilterExpenses={filterExpensesHandler} />
             <ExpenseItem title={props.items[0].title} date={props.items[0].date} amount={props.items[0].amount} />
             <ExpenseItem title={props.items[1].title} date={props.items[1].date} amount={props.items[1].amount}/>
             <ExpenseItem title={props.items[2].title} date={props.items[2].date} amount={props.items[2].amount}/>
