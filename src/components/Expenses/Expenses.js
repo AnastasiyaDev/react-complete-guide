61c70860d56cc0511227e941c07487bf64edf7d5
Asya Bakhnar <abakhnar@oggettoweb.com>
2021-10-06 23:44:39 +0300

    Add uniq key for props.items list, add dynamic expense for onSaveExpense

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -14,9 +14,9 @@ const Expenses = (props) => {
     return (
         <Card className="expenses">
             <ExpensesFilter defaultYear="2020" onFilterExpenses={filterExpensesHandler} />
-            <ExpenseItem title={props.items[0].title} date={props.items[0].date} amount={props.items[0].amount} />
-            <ExpenseItem title={props.items[1].title} date={props.items[1].date} amount={props.items[1].amount}/>
-            <ExpenseItem title={props.items[2].title} date={props.items[2].date} amount={props.items[2].amount}/>
+            {props.items.map((expense) => {
+                return <ExpenseItem key={expense.id} title={expense.title} date={expense.date} amount={expense.amount} />
+            })}
         </Card>
     );
 }
