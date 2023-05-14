9c1de77f209f2393fd6990cf5dca9edca62d0621
Asya Bakhnar <abakhnar@oggettoweb.com>
2023-05-14 21:37:31 +0300

    Add addMovieHandler, push data to firebasedatabase

--- chapter-14/src/App.js
+++ chapter-14/src/App.js
@@ -19,16 +19,18 @@ function App() {
             }
 
             const data = await response.json();
+            const loadedMovies = [];
 
-            const transformedMovies = data.results.map((movieData) => {
-                return {
-                    id: movieData.episode_id,
-                    title: movieData.title,
-                    openingText: movieData.opening_crawl,
-                    releaseDate: movieData.release_date,
-                };
-            });
-            setMovies(transformedMovies);
+            for(const key in data) {
+                loadedMovies.push({
+                    id: key,
+                    title: data[key].title,
+                    openingText: data[key].openingText,
+                    releaseDate: data[key].releaseDate,
+                })
+            }
+
+            setMovies(loadedMovies);
         } catch (error) {
             setError(error.message);
         }
@@ -39,8 +41,15 @@ function App() {
         fetchMoviesHandler();
     }, [fetchMoviesHandler]);
 
-    function addMovieHandler(movie) {
-        console.log(movie);
+    async function addMovieHandler(movie) {
+        const response = await fetch('https://react-http-a4002-default-rtdb.europe-west1.firebasedatabase.app/movies.json', {
+            method: 'POST',
+            body: JSON.stringify(movie),
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        })
+        const data = await response.json();
     }
 
     let content = <p>Found no movies.</p>;
