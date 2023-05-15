35726970554882917906282925fd2f8314709dba
Asya Bakhnar <abakhnar@oggettoweb.com>
2023-05-16 00:19:01 +0300

    Add use-http custom hook

---
+++ chapter-15-example-2/src/hooks/use-http.js
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+
+const useHttp = (requestConfig, applyData) => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const sendRequest = async () => {
+        setIsLoading(true);
+        setError(null);
+
+        const {
+            url,
+            method,
+            body,
+            headers,
+        } = requestConfig;
+
+        try {
+            const response = await fetch(
+                url,
+                {
+                    method: method ?? 'GET',
+                    headers: headers ?? {},
+                    body: body ? JSON.stringify(body) : null,
+                }
+            );
+
+            if (!response.ok) {
+                throw new Error('Request failed!');
+            }
+
+            const data = await response.json();
+
+            applyData(data);
+        } catch (err) {
+            setError(err.message || 'Something went wrong!');
+        }
+        setIsLoading(false);
+    };
+
+    return {
+        isLoading,
+        error,
+        sendRequest,
+    };
+};
+
+export default useHttp;
