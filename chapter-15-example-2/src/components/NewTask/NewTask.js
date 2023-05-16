c7933c6dbf46297f026496557f4034c05fcdb613
Asya Bakhnar <abakhnar@oggettoweb.com>
2023-05-16 22:08:55 +0300

    Add use-http hook to NewTask component (post request)

--- chapter-15-example-2/src/components/NewTask/NewTask.js
+++ chapter-15-example-2/src/components/NewTask/NewTask.js
@@ -1,41 +1,31 @@
-import { useState } from 'react';
-
 import Section from '../UI/Section';
 import TaskForm from './TaskForm';
 
+import useHttp from '../../hooks/use-http';
+
+
 const NewTask = (props) => {
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState(null);
+    const { isLoading, error, sendRequest: saveTasks } = useHttp();
+
 
     const enterTaskHandler = async (taskText) => {
-        setIsLoading(true);
-        setError(null);
-        try {
-            const response = await fetch(
-                'https://react-hooks-254a3-default-rtdb.europe-west1.firebasedatabase.app/tasks.json',
-                {
-                    method: 'POST',
-                    body: JSON.stringify({ text: taskText }),
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                },
-            );
+        const requestConfig = {
+            url: 'https://react-hooks-254a3-default-rtdb.europe-west1.firebasedatabase.app/tasks.json',
+            method: 'POST',
+            body: { text: taskText },
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        }
 
-            if (!response.ok) {
-                throw new Error('Request failed!');
-            }
-
-            const data = await response.json();
-
+        const createTask =(data) => {
             const generatedId = data.name; // firebase-specific => "name" contains generated id
             const createdTask = { id: generatedId, text: taskText };
 
             props.onAddTask(createdTask);
-        } catch (err) {
-            setError(err.message || 'Something went wrong!');
-        }
-        setIsLoading(false);
+        };
+
+        saveTasks(requestConfig, createTask);
     };
 
     return (
