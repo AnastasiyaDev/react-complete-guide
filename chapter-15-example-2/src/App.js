59d8116f88978bb384dd9045902f1193c12e6901
Asya Bakhnar <abakhnar@oggettoweb.com>
2023-05-16 22:02:32 +0300

    Change useHttp hook params, move to sendRequest func

--- chapter-15-example-2/src/App.js
+++ chapter-15-example-2/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback, useMemo } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Tasks from './components/Tasks/Tasks';
 import NewTask from './components/NewTask/NewTask';
@@ -8,30 +8,24 @@ import useHttp from './hooks/use-http';
 function App() {
     const [tasks, setTasks] = useState([]);
 
-    const requestConfig = useMemo(() => {
-        return {
-            url: 'https://react-hooks-254a3-default-rtdb.europe-west1.firebasedatabase.app/tasks.json',
-        }
-    }, []);
-    const transformData = useCallback((data) => {
-        const loadedTasks = [];
-
-        for (const taskKey in data) {
-            loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-        }
-
-        setTasks(loadedTasks);
-    }, []);
-
-    const {
-        isLoading,
-        error,
-        sendRequest: fetchTasks,
-    } = useHttp(requestConfig, transformData);
+    const { isLoading, error, sendRequest: fetchTasks } = useHttp();
 
 
     useEffect(() => {
-        fetchTasks();
+        const requestConfig = {
+            url: 'https://react-hooks-254a3-default-rtdb.europe-west1.firebasedatabase.app/tasks.json',
+        }
+        const transformData = (data) => {
+            const loadedTasks = [];
+
+            for (const taskKey in data) {
+                loadedTasks.push({ id: taskKey, text: data[taskKey].text });
+            }
+
+            setTasks(loadedTasks);
+        };
+
+        fetchTasks(requestConfig, transformData);
     }, [fetchTasks]);
 
     const taskAddHandler = (task) => {
