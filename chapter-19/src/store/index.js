9aa114b9d400fe88f3ef0476f3b2316051c43a5f
Asya Bakhnar <abakhnar@oggettoweb.com>
2024-01-15 13:48:08 +0300

    Splitting Our Code

--- chapter-19/src/store/index.js
+++ chapter-19/src/store/index.js
@@ -1,58 +1,14 @@
-import { createSlice, configureStore } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 
-const initialCounterState = {
-    value: 0,
-    showCounter: true
-};
-
-const counterSlice =  createSlice({
-    name: 'counter',
-    initialState: initialCounterState,
-    reducers: {
-        increment(state) {
-            state.value++
-        },
-        increase(state, action) {
-            state.value += action.payload
-        },
-        decrement(state) {
-            state.value--
-        },
-        toggle(state) {
-            state.showCounter = !state.showCounter
-        },
-    }
-});
-
-
-const initialAuthState = {
-    isAuthentication: false,
-};
-
-const authSlice =  createSlice({
-    name: 'counter',
-    initialState: initialAuthState,
-    reducers: {
-        login(state) {
-            state.isAuthentication = true;
-        },
-        logout(state) {
-            state.isAuthentication = false;
-        },
-    }
-});
-
-
+import counterSlice from './counter';
+import authSlice from './auth';
 
 const store = configureStore({
-    // для множества слайсов, но тогда будет state.counter.showCounter и state.counter.counter
     reducer: {
         counter: counterSlice.reducer,
         auth: authSlice.reducer,
     }
 });
 
-export const counterActions = counterSlice.actions;
-export const authActions = authSlice.actions;
 
 export default store;
