60c1320b1209b05144622a1806aed59a4e7b92bf
Asya Bakhnar <abakhnar@oggettoweb.com>
2024-01-15 13:27:04 +0300

    Working with Multiple Slices

--- chapter-19/src/store/index.js
+++ chapter-19/src/store/index.js
@@ -1,37 +1,58 @@
 import { createSlice, configureStore } from '@reduxjs/toolkit';
 
-const initialState = {
-    counter: 0,
+const initialCounterState = {
+    value: 0,
     showCounter: true
 };
 
 const counterSlice =  createSlice({
     name: 'counter',
-    initialState,
+    initialState: initialCounterState,
     reducers: {
         increment(state) {
-            state.counter++
+            state.value++
         },
         increase(state, action) {
-            state.counter += action.payload
+            state.value += action.payload
         },
         decrement(state) {
-            state.counter--
+            state.value--
         },
         toggle(state) {
-            console.log(state.showCounter);
             state.showCounter = !state.showCounter
         },
     }
-})
+});
+
+
+const initialAuthState = {
+    isAuthentication: 0,
+};
+
+const authSlice =  createSlice({
+    name: 'counter',
+    initialState: initialAuthState,
+    reducers: {
+        login(state) {
+            state.isAuthentication = true;
+        },
+        logout(state) {
+            state.isAuthentication = false;
+        },
+    }
+});
+
+
 
 const store = configureStore({
     // для множества слайсов, но тогда будет state.counter.showCounter и state.counter.counter
-    // reducer: {
-    //     counter: counterSlice.reducer,
-    // }
-    reducer: counterSlice.reducer,
+    reducer: {
+        counter: counterSlice.reducer,
+        auth: authSlice.reducer,
+    }
 });
 
 export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
+
 export default store;
